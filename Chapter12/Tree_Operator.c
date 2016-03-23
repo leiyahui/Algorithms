eb9bfe45da75a1fc055a13b3c7e2a6f88d46a61a
leiyahui <290289286@qq.com>
2016-03-23 10:39:15 +0800

    add Tree_Operator.c

--- Chapter12/Tree_Operator.c
+++ Chapter12/Tree_Operator.c
@@ -6,21 +6,21 @@ typedef struct node
 	node* left;
 	node* right;
 }Node,*NodeList;
-Node Search_Tree(Node node,k)	//Search a Node whose key equals to k in recursion
+Node* Search_Tree(Node* node,k)	//Search a Node whose key equals to k in recursion
 {
 	if(node!=NULL)
 	{
-		if(node.data==k)
+		if(node->data==k)
 		{
 			return node;
 		}
-		else if(node.data<k)
+		else if(node->data<k)
 		{
-			return Tree_Search(*(node.left));
+			return Tree_Search(node.left,k);
 		}
 		else
 		{
-			return Tree_Search(*(node.right));
+			return Tree_Search(node.right,k);
 		}
 	}
 	else
@@ -28,74 +28,74 @@ Node Search_Tree(Node node,k)	//Search a Node whose key equals to k in recursion
 		return NULL;
 	}
 }	
-Node Search_Tree(Node node,k)	//Search a Node whose key equals to k in loop
+Node* Search_Tree(Node* node,k)	//Search a Node whose key equals to k in loop
 {
-	while(node.data!=k&&node!=NULL)
+	while(node->data!=k&&node!=NULL)
 	{
 		if(node.data<k)
 		{
-			node=node.left;
+			node=node->left;
 		}
 		else
 		{
-			node=node.right;
+			node=node->right;
 		}
 		
 	}
 	return node;
 }
-Node Tree_Maximum(Node node)    //return the maximum value of the tree
+Node* Tree_Maximum(Node* node)    //return the maximum value of the tree
 {
-	while(node.right!=NULL)
+	while(node->right!=NULL)
 	{
-		node=node.right;
+		node=node->right;
 	}
 	return node;
 }
-Node Tree_Minimum(Node node)	//return the mimimum value of the tree
+Node* Tree_Minimum(Node* node)	//return the mimimum value of the tree
 {
-	while(node.left!=NULL)
+	while(node->left!=NULL)
 	{
-		node=node.left;
+		node=node->left;
 	}
 	return node;
 }
-Node Tree_Succeosr(Node node)	//retrun the succesor of the node
+Node* Tree_Successor(Node* node)	//retrun the succesor of the node
 {
-	Node y;
-	if(node.right!=NULL)
+	Node* y;
+	if(node->right!=NULL)
 	{
-		return Tree_Minimum(*(node.right));
+		return Tree_Minimum(node->right);
 	}
-	y=node.p;
-	while(y!=NULL&&node==y.right)
+	y=node->p;
+	while(y!=NULL&&node==y->right)
 	{
 		node=y;
-		y=*(node.p);
+		y=node.p;
 	}
 	return y;
 }
-Node Tree_Predecessor(Node node)		//return the predecessor of the node
+Node* Tree_Predecessor(Node* node)		//return the predecessor of the node
 {
-	Node y;
-	if(node.left!=NULL)
+	Node* y;
+	if(node->left!=NULL)
 	{
-		return Tree_Maximum(*(node.left));
+		return Tree_Maximum(node->left);
 	}
-	y=node.p;
-	while(y!=NULL&&node==y.left)
+	y=node->p;
+	while(y!=NULL&&node==y->left)
 	{
 		node=y;
-		y=*(node.p);
+		y=node->p;
 	}
 	return y;
 }
-void Tree_Insert(Node T,int z)		//insert x to T
+void Tree_Insert(Node* T,int z)		//insert x to T
 {
 	Node nodeX;
 	Node nodeY;
 	NodeList nList;
-	nodeX=T;
+	nodeX=*T;
 	nodeY=NULL;
 	while(X!=NULL)
 	{
@@ -122,15 +122,48 @@ void Tree_Insert(Node T,int z)		//insert x to T
 		nodeY.right=nList;
 	}
 }
-void Transplant(Node T,Node x,Node y)
+void Transplant(Node* x,Node* y)
 {
-
-void Tree_Delete(Node T,int z)		//Delete x from T
-{
-	if(T.left==NULL)
+	if((x->p)->left==x)
 	{
-				 	
-		
+		(x->p)->left=y;
+	}
+	else
+	{
+		(x->p)->right=y;
+	}
+	if(y!=NULL)
+	{
+		y->p=x->p;
+	}
+}
+	
+void Tree_Delete(Node* x)		//Delete x from T
+{
+	Node* node_ptr;
+	if(x->left==NULL)
+	{
+		Transplant(x,x->right);
+	}
+	else if(x->right==NULL)
+	{
+		Transplant(x,x->left);
+	}
+	else
+	{
+		node_ptr=Tree_Soccessor(x);
+		if(node_ptr.p!=x)
+		{
+			Transplant(node_ptr,node_ptr.right);  //replace node_ptr while node_ptr's right child
+			node_ptr->right=x->right;		//change z's right child into y's rightchild
+			(node_ptr->right)->p=node_ptr;
+		}
+		Trasnplant(x,node_ptr);
+		node_ptr->left=x->left;
+		(node_ptr->left)->p=node_ptr;
+	}	
+}				 	
+
 
 
 
