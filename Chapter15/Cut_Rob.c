fc03870c0f5dd40050d84d48dc8d78eeb6603144
leiyahui <290289286@qq.com>
2016-03-27 10:01:54 +0800

    add

---
+++ Chapter15/Cut_Rob.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int cut_rob(int A[],int n)
+{
+	int i,temp;
+	int q=-10000	;	//as minimum as possible
+	if(n==0)
+	{
+		return 0;
+	}
+	for(i=1;i<=n;i++)
+	{
+		temp=A[i]+cut_rob(A,n-i);
+		if(q<temp)
+		{
+			q=temp;	
+		}
+	}
+	return q;
+}	
+void main()
+{
+	int A[10];
+	int n;
+	int q;
+	A[1]=1;
+	A[2]=5;
+	A[3]=8;
+	A[4]=9;
+	A[5]=10;
+	A[6]=17;
+	A[8]=20;
+	A[9]=24;
+	A[10]=30;
+	printf("Please input n\n");
+	scanf("%d",&n);
+	q=cut_rob(A,n); 
+	printf("%d\n",q);
+}
