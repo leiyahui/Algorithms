95651e106077fecbae7ecddf1aede5efdc7c45bd
leiyahui <290289286@qq.com>
2016-03-27 10:50:46 +0800

    change Cut_Rob.c

--- Chapter15/Cut_Rob.c
+++ Chapter15/Cut_Rob.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int cut_rob(int A[],int n)
+int cut_rob(int A[],int n,int c)
 {
 	int i,temp;
 	int q=-10000	;	//as minimum as possible
@@ -7,12 +7,12 @@ int cut_rob(int A[],int n)
 	{
 		return 0;
 	}
-	for(i=1;i<=n;i++)
+	for(i=0;i<n;i++)
 	{
-		temp=A[i]+cut_rob(A,n-i);
+		temp=A[i+1]+cut_rob(A,n-i-1,c+1);
 		if(q<temp)
 		{
-			q=temp;	
+			q=temp;
 		}
 	}
 	return q;
@@ -22,6 +22,7 @@ void main()
 	int A[10];
 	int n;
 	int q;
+	int i;
 	A[1]=1;
 	A[2]=5;
 	A[3]=8;
@@ -33,6 +34,6 @@ void main()
 	A[10]=30;
 	printf("Please input n\n");
 	scanf("%d",&n);
-	q=cut_rob(A,n); 
+	q=cut_rob(A,n,0); 
 	printf("%d\n",q);
 }
